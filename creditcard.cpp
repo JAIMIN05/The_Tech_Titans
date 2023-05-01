fd0a3659c6c79b08639554971635ac2096692836
22CS020samarthdhol <120238118+22CS020samarthdhol@users.noreply.github.com>
2023-05-01 18:57:03 +0530

    Add files via upload

--- creditcard.cpp
+++ creditcard.cpp
@@ -3,13 +3,13 @@
 using namespace std;
 
 //Create a function to check if a credit card number is valid
-bool isCreditCardValid(string cardNumber)
+bool is_Credit_Card_Valid(string cardNum)
 {
     int sum = 0; //Initialize the sum variable to zero
     bool alternate = false; //Initialize the alternate variable to false
-    for (int i = cardNumber.length() - 1; i >= 0; i--)
+    for (int i = cardNum.length() - 1; i >= 0; i--)
     {
-        int digit = cardNumber[i] - '0'; // Convert the character digit to an integer
+        int digit = cardNum[i] - '0'; // Convert the character digit to an integer
         if (alternate)
         {
             digit *= 2; // Double the digit if it is in an alternate position
@@ -26,31 +26,40 @@ bool isCreditCardValid(string cardNumber)
 
 int main()
 {
-    string cardNumber; // Declare a string variable to store the credit card number
+    string cardNum; // Declare a string variable to store the credit card number
     cout << "Enter credit card number: ";
-    cin >> cardNumber; // Read the credit card number from the user
-    if (isCreditCardValid(cardNumber)) // If the credit card number is valid, print a message
+    cin >> cardNum; // Read the credit card number from the user
+    if (is_Credit_Card_Valid(cardNum)) // If the credit card number is valid, print a message
     {
-        cout<<"-------------------------------------------------"<<endl;
-        cout <<endl<< "CREDIT CARD NUMBER IS VALID." << endl <<endl;
-        cout<<"-------------------------------------------------"<<endl;
+        cout<<endl<<"*************************************************"<<endl;
+        cout<< "\"CREDIT CARD NUMBER IS VALID.\""<< endl;
+        cout<<"*************************************************"<<endl;
     }
     else // Otherwise, print a different message
     {
-        cout<<"-------------------------------------------------"<<endl;
-        cout <<endl<< "CREDIT CARD NUMBER IS NOT VALID." << endl << endl;
-        cout<<"-------------------------------------------------"<<endl;
+        cout<<endl<<"*************************************************"<<endl;
+        cout<< "\"CREDIT CARD NUMBER IS NOT VALID.\""<< endl;
+        cout<<"*************************************************"<<endl;
     }
-    cout<<endl<<endl<<"*************************************************"<<endl;
-    cout<<"* THIS PROGRAM IS PREPARED BY:-"<<"                 *"<<endl;
-    cout<<"* NAME--SAMARTH DHOL, ID---22CS020"<<"              *"<<endl;
-    cout<<"* NAME--JAIMIN KANERIYA, ID---22CS028"<<"           *"<<endl;
-    cout<<"* NAME--NISHIT GAJIPARA, ID---22CS022"<<"           *"<<endl;
-    cout<<"*************************************************"<<endl;
-  return 0;
+    cout<<endl<<endl<<"-------------------------------------------------"<<endl;
+    cout<<"| THIS PROGRAM IS PREPARED BY:-"<<"                 |"<<endl;
+    cout<<"| NAME--SAMARTH DHOL, ID---22CS020"<<"              |"<<endl;
+    cout<<"| NAME--JAIMIN KANERIYA, ID---22CS028"<<"           |"<<endl;
+    cout<<"| NAME--NISHIT GAJIPARA, ID---22CS022"<<"           |"<<endl;
+    cout<<"-------------------------------------------------"<<endl;
+    return 0;
 }
-/*This program created by
- 22cs028 -> jaimin kaneriya
- 22cs020 -> samarth Dhol
- 22cs022 -> Nishit gajipara
+/*VALID CREDIT CARD NUMBERS OF INPUT
+American Express ---> 371449635398431
+Diners Club      ---> 30569309025904
+Discover         ---> 6011111111111117
+JCB              ---> 3530111333300000
+MasterCard       ---> 5555555555554444
+Visa             ---> 4111111111111111
+*/
+
+/*THIS PROGRAM CREATED BY
+ 22CS028 -> JAIMIN KANERIYA
+ 22CS020 -> SAMARTH DHOL
+ 22CS022 -> NISHIT GAJIPARA
 */
