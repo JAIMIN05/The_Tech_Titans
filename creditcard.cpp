5faf9438b386884c2404a7c3e27e928d731dd4c8
22CS020samarthdhol <120238118+22CS020samarthdhol@users.noreply.github.com>
2023-05-03 23:39:33 +0530

    Update creditcard.cpp

--- creditcard.cpp
+++ creditcard.cpp
@@ -4,10 +4,10 @@
 using namespace std;
 
 //Create a function to check if a credit card number is valid
-bool is_Credit_Card_Valid(string cardNum)
+int is_Credit_Card_Valid(string cardNum)
 {
-    int sum = 0; //Initialize the sum variable to zero
-    bool alternate = false; //Initialize the alternate variable to false
+    float sum = 0; //Initialize the sum variable to zero
+    int alternate = 1; //Initialize the alternate variable to false
     for (int i = cardNum.length() - 1; i >= 0; i--)
     {
         int digit = cardNum[i] - '0'; // Convert the character digit to an integer
@@ -20,23 +20,37 @@ bool is_Credit_Card_Valid(string cardNum)
             }
         }
         sum += digit; // Add the digit to the sum variable
-        alternate = !alternate; // Alternate the value of alternate
+        if(i%2==0)
+        {
+            alternate = 1; // Alternate the value of alternate
+        }
+        else
+        {
+            alternate = 0;
+        }
     }
     return (sum % 10 == 0); // If the sum is divisible by 10, return true. Otherwise, return false.
 }
 
 int main()
 {
-    string cardNum; // Declare a string variable to store the credit card number
-    cout << "Enter credit card number: ";
+    string cardNum; // Declare a string variable to store the credit card numbe
     cin >> cardNum; // Read the credit card number from the user
     if (is_Credit_Card_Valid(cardNum)) // If the credit card number is valid, print a message
     {
+        cout<<"---------------------------------------------"<<endl;
+        cout<<"---------------------------------------------"<<endl;
         cout<< "CREDIT CARD NUMBER IS VALID."<< endl;
+        cout<<"---------------------------------------------"<<endl;
+        cout<<"---------------------------------------------"<<endl;
     }
     else // Otherwise, print a different message
     {
+        cout<<"---------------------------------------------"<<endl;
+        cout<<"---------------------------------------------"<<endl;
         cout<< "CREDIT CARD NUMBER IS NOT VALID."<< endl;
+        cout<<"---------------------------------------------"<<endl;
+        cout<<"---------------------------------------------"<<endl;
     }
     return 0;
 }
