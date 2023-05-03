49f689709d2b40deb2a1dab30364c41b24de8f2d
JAIMIN05 <115476522+JAIMIN05@users.noreply.github.com>
2023-05-03 23:22:14 +0530

    Add files via upload

--- creditcard.cpp
+++ creditcard.cpp
@@ -1,65 +1,40 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
-//Create a function to check if a credit card number is valid
-bool is_Credit_Card_Valid(string cardNum)
+bool isCreditCardValid(string cardNumber)
 {
-    int sum = 0; //Initialize the sum variable to zero
-    bool alternate = false; //Initialize the alternate variable to false
-    for (int i = cardNum.length() - 1; i >= 0; i--)
+    int sum = 0;
+    bool alternate = true; 
+    for (int i = cardNumber.length() - 1; i > 0; i--)
     {
-        int digit = cardNum[i] - '0'; // Convert the character digit to an integer
+        int digit = cardNumber[i] - '0'; 
         if (alternate)
         {
-            digit *= 2; // Double the digit if it is in an alternate position
+            digit *= 2; 
             if (digit > 9)
             {
-                digit = (digit % 10) + 1; // If the doubled digit is greater than 9, add its digits together
+                digit = (digit % 10) + 1;
             }
         }
-        sum += digit; // Add the digit to the sum variable
-        alternate = !alternate; // Alternate the value of alternate
+        sum += digit; 
+        alternate = !alternate; 
     }
-    return (sum % 10 == 0); // If the sum is divisible by 10, return true. Otherwise, return false.
+    return (sum % 10 == 0); 
 }
 
 int main()
 {
-    string cardNum; // Declare a string variable to store the credit card number
+    string cardNumber; 
     cout << "Enter credit card number: ";
-    cin >> cardNum; // Read the credit card number from the user
-    if (is_Credit_Card_Valid(cardNum)) // If the credit card number is valid, print a message
+    cin >> cardNumber; 
+    if (isCreditCardValid(cardNumber)) 
     {
-        cout<<endl<<"*************************************************"<<endl;
-        cout<< "\"CREDIT CARD NUMBER IS VALID.\""<< endl;
-        cout<<"*************************************************"<<endl;
+        cout << "Credit card number is valid." << endl;
     }
     else // Otherwise, print a different message
     {
-        cout<<endl<<"*************************************************"<<endl;
-        cout<< "\"CREDIT CARD NUMBER IS NOT VALID.\""<< endl;
-        cout<<"*************************************************"<<endl;
+        cout << "Credit card number is not valid." << endl;
     }
-    cout<<endl<<endl<<"-------------------------------------------------"<<endl;
-    cout<<"| THIS PROGRAM IS PREPARED BY:-"<<"                 |"<<endl;
-    cout<<"| NAME--SAMARTH DHOL, ID---22CS020"<<"              |"<<endl;
-    cout<<"| NAME--JAIMIN KANERIYA, ID---22CS028"<<"           |"<<endl;
-    cout<<"| NAME--NISHIT GAJIPARA, ID---22CS022"<<"           |"<<endl;
-    cout<<"-------------------------------------------------"<<endl;
-    return 0;
+  return 0;
 }
-/*VALID CREDIT CARD NUMBERS FOR INPUT
-American Express ---> 371449635398431
-Diners Club      ---> 30569309025904
-Discover         ---> 6011111111111117
-JCB              ---> 3530111333300000
-MasterCard       ---> 5555555555554444
-Visa             ---> 4111111111111111
-*/
 
-/*THIS PROGRAM CREATED BY
- 22CS028 -> JAIMIN KANERIYA
- 22CS020 -> SAMARTH DHOL
- 22CS022 -> NISHIT GAJIPARA
-*/
