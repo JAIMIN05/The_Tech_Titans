6953f07c1b12e5f2b93a87fd8f70b44960211ea8
22CS020samarthdhol <120238118+22CS020samarthdhol@users.noreply.github.com>
2023-05-03 23:46:22 +0530

    Add files via upload

--- creditcard.cpp
+++ creditcard.cpp
@@ -2,39 +2,40 @@
 #include <string>
 using namespace std;
 
+//Create a function to check if a credit card number is valid
 bool is_Credit_Card_Valid(string cardNum)
 {
-    int sum = 0; 
-    bool alternate = false; 
+    int sum = 0; //Initialize the sum variable to zero
+    bool alternate = false; //Initialize the alternate variable to false
     for (int i = cardNum.length() - 1; i >= 0; i--)
     {
-        int digit = cardNum[i] - '0';
+        int digit = cardNum[i] - '0'; // Convert the character digit to an integer
         if (alternate)
         {
-            digit *= 2;
+            digit *= 2; // Double the digit if it is in an alternate position
             if (digit > 9)
             {
-                digit = (digit % 10) + 1; 
+                digit = (digit % 10) + 1; // If the doubled digit is greater than 9, add its digits together
             }
         }
-        sum += digit; 
-        alternate = !alternate;
+        sum += digit; // Add the digit to the sum variable
+        alternate = !alternate; // Alternate the value of alternate
     }
-    return (sum % 10 == 0); 
+    return (sum % 10 == 0); // If the sum is divisible by 10, return true. Otherwise, return false.
 }
 
 int main()
 {
-    string cardNum; 
+    string cardNum; // Declare a string variable to store the credit card number
     cout << "Enter credit card number: ";
-    cin >> cardNum; 
-    if (is_Credit_Card_Valid(cardNum)) 
+    cin >> cardNum; // Read the credit card number from the user
+    if (is_Credit_Card_Valid(cardNum)) // If the credit card number is valid, print a message
     {
         cout<<endl<<"*************************************************"<<endl;
         cout<< "\"CREDIT CARD NUMBER IS VALID.\""<< endl;
         cout<<"*************************************************"<<endl;
     }
-    else 
+    else // Otherwise, print a different message
     {
         cout<<endl<<"*************************************************"<<endl;
         cout<< "\"CREDIT CARD NUMBER IS NOT VALID.\""<< endl;
@@ -48,4 +49,17 @@ int main()
     cout<<"-------------------------------------------------"<<endl;
     return 0;
 }
+/*VALID CREDIT CARD NUMBERS FOR INPUT
+American Express ---> 371449635398431
+Diners Club      ---> 30569309025904
+Discover         ---> 6011111111111117
+JCB              ---> 3530111333300000
+MasterCard       ---> 5555555555554444
+Visa             ---> 4111111111111111
+*/
 
+/*THIS PROGRAM CREATED BY
+ 22CS028 -> JAIMIN KANERIYA
+ 22CS020 -> SAMARTH DHOL
+ 22CS022 -> NISHIT GAJIPARA
+*/
